74ea0c552980e2ad115ca87c9bb9b50833a0b4df
Hasnain Javed <hkhan45@gmail.com>
2014-10-09 14:07:08 +0500

    removed unused imports and commented code

--- src/main/java/org/springframework/data/crate/core/mapping/CratePersistentEntityTableDefinitionMapper.java
+++ src/main/java/org/springframework/data/crate/core/mapping/CratePersistentEntityTableDefinitionMapper.java
@@ -16,12 +16,9 @@
 package org.springframework.data.crate.core.mapping;
 
 import static java.lang.Boolean.TRUE;
-import static java.util.Collections.emptyList;
 import static org.slf4j.LoggerFactory.getLogger;
-import static org.springframework.data.crate.core.mapping.CrateDataType.ARRAY;
 import static org.springframework.data.crate.core.mapping.CrateDataType.getCrateTypeFor;
 import static org.springframework.data.util.ClassTypeInformation.from;
-import static org.springframework.util.Assert.hasText;
 import static org.springframework.util.Assert.notNull;
 
 import java.util.ArrayList;
@@ -100,94 +97,6 @@ public class CratePersistentEntityTableDefinitionMapper implements TableDefiniti
 		return mappingContext.isSimpleType(collectionTypeProperty.getComponentType());
 	}
 
-	/*public static class TableDefinition {
-		
-		private String name;
-		
-		private List<Column> columns;
-
-		public String getName() {
-			return name;
-		}
-		
-		public void setName(String name) {
-			hasText(name);
-			this.name = name;
-		}
-		
-		public List<Column> getColumns() {
-			return columns;
-		}
-		
-		public void setColumns(List<Column> columns) {
-			notNull(columns);
-			this.columns = columns;
-		}
-	}*/
-	
-	/*public static class Column {
-		
-		private String name;
-		private String type;
-		private String elementType;
-		
-		private boolean primaryKey;
-		
-		private List<Column> subColumns;
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			hasText(name);
-			this.name = name;
-		}
-
-		public String getType() {
-			return type;
-		}
-
-		public void setType(String type) {
-			hasText(type);
-			this.type = type;
-		}
-		
-		public String getElementType() {
-			return elementType;
-		}
-
-		public void setElementType(String elementType) {
-			hasText(elementType);
-			this.elementType = elementType;
-		}
-
-		public boolean isPrimaryKey() {
-			return primaryKey;
-		}
-
-		public void setPrimaryKey(boolean primaryKey) {
-			this.primaryKey = primaryKey;
-		}
-
-		public List<Column> getSubColumns() {
-			
-			if(subColumns == null) {
-				return emptyList();
-			}
-			
-			return subColumns;
-		}
-
-		public void setSubColumns(List<Column> subColumns) {
-			this.subColumns = subColumns;
-		}
-
-		public boolean isArrayColumn() {
-			return ARRAY.equalsIgnoreCase(type);
-		}
-	}*/
-	
 	/**
 	 * 
 	 * @author Hasnain Javed
